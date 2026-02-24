65a899d1bb104ebbc8f95e9e17a45f8a3b316c26
nitesh.dev <nitesh.dev@proton.me>
2026-02-24 23:16:21 +0530

    feat: support of commonjs

---
+++ index.cjs
@@ -0,0 +1,13 @@
+const fs = require("node:fs");
+function pathKiller([dirURL, path]) {
+    console.log(`release: v1.1.0`);
+    // 1. path and dirlist declaration
+    // 2. reading wordlist file
+    // 3. loop through words
+    (fs.readFileSync(`${dirURL}`).toString()).split("\n").forEach(async (dir, i) => {
+        let reponse = await fetch(`${path}/${dir}`);
+        console.log(`URL: ${reponse.url}\nRESPONSE CODE::${reponse.status}\n`);
+    });
+}
+
+module.exports = pathKiller;
